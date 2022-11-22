6fd961e8c3ab7fe625ea4a668cff95cdde46660e
rich-cherry <rich.m.cherry@gmail.com>
2022-11-22 14:32:37 -0500

    Reformat

--- animation/index.js
+++ animation/index.js
@@ -1,23 +1,25 @@
-const observerOptions = {
+const animationObserverOptions = {
   root: null,
   threshold: 0,
-  rootMargin: '0px 0px -100px 0px'
-};
+  rootMargin: '0px 0px -200px 0px'
+}
 
-const observer = new IntersectionObserver(entries => {
+const animationObserver = new IntersectionObserver(entries => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
-      entry.target.querySelector('h1').setAttribute('data-in-view', 'true');
-      entry.target.querySelector('h2').setAttribute('data-in-view', 'true');
-      entry.target.querySelector('h3').setAttribute('data-in-view', 'true');
-      observer.unobserve(entry.target);
+      entry.target.querySelectorAll('[data-animation]').forEach(el => {
+        let delay = Number.parseInt(el.dataset.animationDelay || 0)
+        el.style.animationDelay = delay + "ms"
+        el.setAttribute('data-in-view', 'true')
+      })
+
+      animationObserver.unobserve(entry.target)
     }
   });
-}, observerOptions);
+}, animationObserverOptions)
 
+const animationElements = document.querySelectorAll('[data-animate="true"]')
 
-const elements = document.querySelectorAll('[data-animate="true"]');
-elements.forEach(element => {
-  observer.observe(element);
+animationElements.forEach(element => {
+  animationObserver.observe(element)
 })
-
